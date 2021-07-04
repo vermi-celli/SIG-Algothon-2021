573109084d90d9aa736955c3f204379737f8572f
vermi-celli <86867543+vermi-celli@users.noreply.github.com>
2021-07-04 20:43:32 +1000

    Update and rename reading-data.ipynb to reading-data.py

---
+++ reading-data.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+days = []
+f = open('/Users/ivykim/Downloads/algothon21-main/prices250.txt')
+    #days = f.readlines()
+
+listlist = []
+for line in f:
+     Day = line.strip()
+     Price_Day = Day.split()
+     listlist.append(Price_Day)
+
+Progress = []
+Stock = 0 #choose the instrument
+for list in listlist:
+    Price = list[Stock]
+    Progress.append(Price)
+
+Stockmove = []
+for str in Progress:
+    Price = float(str)
+    Stockmove.append(Price)
+
+Daylist = range(1, 251)
+fig, ax = plt.subplots(figsize=(10,6))
+ax.plot(Daylist,
+        Stockmove)
+
+#count=0
+#for day in days: 
+    #count += 1
+    #print(f'day {count}: {day}')
+
+#listlist[1] gets 100 instruments on first day
