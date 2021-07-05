66c9ea5c41bd3f19347301fbf13a36e54e26af43
vermi-celli <86867543+vermi-celli@users.noreply.github.com>
2021-07-06 00:33:14 +1000

    Update single-graph.py

--- Make-graph/single-graph.py
+++ Make-graph/single-graph.py
@@ -6,36 +6,21 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-days = []
 f = open('/Users/ivykim/Downloads/algothon21-main/prices250.txt')
-    #days = f.readlines()
 
-listlist = []
-for line in f:
-     Day = line.strip()
-     Price_Day = Day.split()
-     listlist.append(Price_Day)
-
-Progress = []
-Stock = 0 #choose the instrument
-for list in listlist:
-    Price = list[Stock]
-    Progress.append(Price)
-
-Stockmove = []
-for str in Progress:
-    Price = float(str)
-    Stockmove.append(Price)
+Stock = input("Enter stock number: ")
+Stock = int(Stock)
+Dataset = []
+for X in f:
+     Day = X.strip().split()
+     Dataset.append(Day)
+Stockprice = []
+for X in Dataset:
+    Day_Price = float(X[Stock])
+    Stockprice.append(Day_Price)
 
 Daylist = range(1, 251)
-plt.plot(Daylist, Stockmove)
+plt.plot(Daylist, Stockprice)
 plt.xlabel('Day')
 plt.ylabel('Price')
 plt.show()
-
-#count=0
-#for day in days: 
-    #count += 1
-    #print(f'day {count}: {day}')
-
-#listlist[1] gets 100 instruments on first day
